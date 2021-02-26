15f9e7a95d27432a78e1a3cd0a023a9b148a7dd6
loggreev <70668891+loggreev@users.noreply.github.com>
2021-02-25 19:06:04 -0500

    implement selection buttons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import SelectionButton from './components/SelectionButton.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SelectionButton type="rock" />
+      <SelectionButton type="rock" />
+      <SelectionButton type="paper" />
+      <SelectionButton type="scissors" />
+      <SelectionButton type="lizard" />
+      <SelectionButton type="spock" />
     </div>
   );
 }
---
+++ src/components/SelectionButton.js
@@ -0,0 +1,41 @@
+import './SelectionButton.css'
+import rock from './../images/icon-rock.svg'
+import paper from './../images/icon-paper.svg'
+import scissors from './../images/icon-scissors.svg'
+import lizard from './../images/icon-lizard.svg'
+import spock from './../images/icon-spock.svg'
+
+function SelectionButton(props) {
+    const type = props.type;
+    let svg;
+    switch (type) {
+        case 'rock':
+            svg = rock;
+            break;
+        case 'paper':
+            svg = paper;
+            break;
+        case 'scissors':
+            svg = scissors;
+            break;
+        case 'lizard':
+            svg = lizard;
+            break;
+        case 'spock':
+            svg = spock;
+            break;
+        default: ;
+    }
+
+    return (
+        <div className="SelectionButton">
+            <div className={"circle outer " + type}>
+                <div className="circle inner">
+                    <img src={svg} alt="" />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default SelectionButton;
