2f1107dc3555ca4aac4dcea69b68bf2adec2d586
loggreev <70668891+loggreev@users.noreply.github.com>
2021-02-25 19:15:06 -0500

    add selection button container

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import SelectionButton from './components/SelectionButton.js';
 function App() {
   return (
     <div className="App">
-      <SelectionButton type="rock" />
-      <SelectionButton type="rock" />
-      <SelectionButton type="paper" />
-      <SelectionButton type="scissors" />
-      <SelectionButton type="lizard" />
-      <SelectionButton type="spock" />
+      <div className="SelectionButton-container">
+        <SelectionButton type="rock" />
+        <SelectionButton type="paper" />
+        <SelectionButton type="scissors" />
+        <SelectionButton type="lizard" />
+        <SelectionButton type="spock" />
+      </div>
     </div>
   );
 }
