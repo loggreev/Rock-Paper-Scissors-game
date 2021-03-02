f1c52399963f826e80a81016bc2ee36f4c70fe17
loggreev <70668891+loggreev@users.noreply.github.com>
2021-03-02 16:18:22 -0500

    implement desktop/responsive layout

--- src/components/SelectionButtonContainer.js
+++ src/components/SelectionButtonContainer.js
@@ -4,17 +4,19 @@ import SelectionButton from './SelectionButton.js'
 function SelectionButtonContainer(props) {
     return (
         <div className="SelectionButton-container">
-            <div className="row-container">
-                <div className="row">
-                    <SelectionButton type="scissors" onclick={props.selectChoice} />
-                </div>
-                <div className="row">
-                    <SelectionButton type="spock" onclick={props.selectChoice} />
-                    <SelectionButton type="paper" onclick={props.selectChoice} />
-                </div>
-                <div className="row">
-                    <SelectionButton type="lizard" onclick={props.selectChoice} />
-                    <SelectionButton type="rock" onclick={props.selectChoice} />
+            <div className="container">
+                <div className="row-container">
+                    <div className="row">
+                        <SelectionButton type="scissors" onclick={props.selectChoice} />
+                    </div>
+                    <div className="row">
+                        <SelectionButton type="spock" onclick={props.selectChoice} />
+                        <SelectionButton type="paper" onclick={props.selectChoice} />
+                    </div>
+                    <div className="row">
+                        <SelectionButton type="lizard" onclick={props.selectChoice} />
+                        <SelectionButton type="rock" onclick={props.selectChoice} />
+                    </div>
                 </div>
             </div>
         </div>
